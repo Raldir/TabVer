89efb176760728b37135d1c0f0636acd80f1162c
Rami <rami-aly@hotmail.de>
2025-03-16 12:56:09 +0000

    Update download_models.sh

--- bin/download_models.sh
+++ bin/download_models.sh
@@ -7,4 +7,5 @@ gdown --folder https://drive.google.com/drive/folders/16NSIq9CTXdtYJ420_t_prynnZ
 gdown --folder https://drive.google.com/drive/folders/1GAYB1RD0gtk_10ZMp2ryhLkOjb8-Fn2r?usp=sharing -O models/Mistral-7B-OpenOrca-LoRA-QA
 gdown --folder https://drive.google.com/drive/folders/1giQTVhupb7nWMtoIMCZJgcx2CPOikFSr?usp=sharing -O models/Mistral-7B-OpenOrca-LoRA-Decomp
 gdown --folder https://drive.google.com/drive/folders/10kmq-XmyEvqsMme2J3GxIxX6KALSiziK?usp=sharing -O models/awesomealign/paper
+gdown --folder https://drive.google.com/drive/folders/1UbOW8CXT1cgpT54cMcr1ettFfzdIngp1?usp=sharing -O models/TabVer_FlanT5-xl/42
 
